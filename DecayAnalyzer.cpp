61af109dc6bb6bb168ec0280a24ad617fe8078bb
agustus <ballachouaib14@gmail.com>
2024-12-14 20:03:36 +0100

    Update DecayAnalyzer.cpp

--- DecayAnalyzer.cpp
+++ DecayAnalyzer.cpp
@@ -11,10 +11,10 @@ double calculateNt(double n0, double lambda, double t);  // N(t)
 double calculateMt(double m0, double lambda, double t);  // M(t)
 
 int main() {
-    int choice;  // Variable to store the user's choice
-    double t;    // Variable to store the time value (t)
+    int choice;  
+    double t;    // Variable to store the time value 
 
-    // Display welcome message and choices
+    
     std::cout << "Welcome to the nuclear calculations :)\n";
     std::cout << "Choose one of the following calculations:\n";
     std::cout << "1. A(t) (Activity)\n";
@@ -23,37 +23,37 @@ int main() {
     std::cout << "Enter your choice (1-3): ";
     std::cin >> choice;
 
-    // Ask for lambda (decay constant) and time (t), which are needed for all calculations
+  
     std::cout << "Enter lambda (decay constant): ";
-    std::cin >> inputs[3];  // Store lambda in position 3 of the inputs array
+    std::cin >> inputs[3]; 
     std::cout << "Enter time (t) in seconds: ";
-    std::cin >> t;  // Store time in variable t
-
+    std::cin >> t;  
+    
     switch (choice) {
-        case 1: // If the user chooses A(t)
+        case 1: // user chooses A(t)
             std::cout << "You are calculating A(t)\n";
             std::cout << "Enter A0 (initial activity): ";
-            std::cin >> inputs[0];  // Store A0 in position 0 of the inputs array
+            std::cin >> inputs[0];  
 
-            results[0] = calculateAt(inputs[0], inputs[3], t);  // Calculate A(t)
+            results[0] = calculateAt(inputs[0], inputs[3], t);  
             std::cout << "The value of A(t): " << results[0] << " units\n";
             break;
 
-        case 2: // If the user chooses N(t)
+        case 2: //  the user chooses N(t)
             std::cout << "You are calculating N(t)\n";
             std::cout << "Enter N0 (initial number): ";
-            std::cin >> inputs[1];  // Store N0 in position 1 of the inputs array
+            std::cin >> inputs[1]; 
 
-            results[1] = calculateNt(inputs[1], inputs[3], t);  // Calculate N(t)
+            results[1] = calculateNt(inputs[1], inputs[3], t);  
             std::cout << "The value of N(t): " << results[1] << " units\n";
             break;
 
         case 3: // If the user chooses M(t)
             std::cout << "You are calculating M(t)\n";
             std::cout << "Enter M0 (initial mass): ";
-            std::cin >> inputs[2];  // Store M0 in position 2 of the inputs array
+            std::cin >> inputs[2];  
 
-            results[2] = calculateMt(inputs[2], inputs[3], t);  // Calculate M(t)
+            results[2] = calculateMt(inputs[2], inputs[3], t);  
             std::cout << "The value of M(t): " << results[2] << " units\n";
             break;
 
@@ -67,17 +67,17 @@ int main() {
 
 // Function to calculate A(t) and return the result
 double calculateAt(double a0, double lambda, double t) {
-    return a0 * std::exp(-lambda * t);  // Calculate and return A(t)
+    return a0 * std::exp(-lambda * t);  
 }
 
 // Function to calculate N(t) and return the result
 double calculateNt(double n0, double lambda, double t) {
-    return n0 * std::exp(-lambda * t);  // Calculate and return N(t)
+    return n0 * std::exp(-lambda * t);  
 }
 
 // Function to calculate M(t) and return the result
 double calculateMt(double m0, double lambda, double t) {
-    return m0 * std::exp(-lambda * t);  // Calculate and return M(t)
+    return m0 * std::exp(-lambda * t);  
 }
 
 
